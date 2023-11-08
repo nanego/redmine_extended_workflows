56c246dc3c2fbe9e2277771dd78e145ce8af89a9
Yalaeddin <y.alaeddin@adacis.net>
2023-11-08 17:16:53 +0100

    Créer la table WorkflowProject + le model

---
+++ app/models/workflow_project.rb
@@ -0,0 +1,31 @@
+class WorkflowProject < ActiveRecord::Base
+
+  belongs_to :role
+  validates_presence_of :role
+
+  def self.rules_by_roles(roles)
+    WorkflowProject.where(:role_id => roles.map(&:id)).inject({}) do |h, w|
+      h[w.role_id] ||= {}
+      h[w.role_id][w.field_name] ||= []
+      h[w.role_id][w.field_name] << w.rule
+      h
+    end
+  end
+
+  def self.replace_permissions(roles, permissions)
+    roles = Array.wrap roles
+
+    transaction do
+      permissions&.each do |role_id, rule_by_field|
+        rule_by_field&.each do |field, rule|
+          where(:role_id => role_id, :field_name => field).destroy_all
+
+          if rule.present?
+            WorkflowProject.create!(:role_id => role_id, :field_name => field, :rule => rule)
+          end
+        end
+      end
+    end
+  end
+
+end
