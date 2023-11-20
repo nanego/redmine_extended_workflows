36104f009fbbdc6ed12571a4b8b7d7c544bb4637
Vincent Robert <vincent.robert@nanego.com>
2023-11-20 11:12:25 +0100

    Add README file and CI configuration

--- init.rb
+++ init.rb
@@ -3,8 +3,10 @@ require_relative 'lib/redmine_extended_workflows/hooks'
 Redmine::Plugin.register :redmine_extended_workflows do
   name 'Redmine Extended Workflows plugin'
   author 'Vincent ROBERT'
-  description 'TODO'
+  description 'This plugin enhances standard workflows by introducing new configuration options.'
   version '0.0.1'
   url 'https://github.com/nanego/redmine_extended_workflows'
+
+  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
 
 end
