f74129804363d286a479be915d31b4bbfdc36e75
Vincent Robert <vincent.robert@nanego.com>
2023-11-30 16:47:15 +0100

    Simplify Project patch

--- lib/redmine_extended_workflows/models/project_patch.rb
+++ lib/redmine_extended_workflows/models/project_patch.rb
@@ -10,45 +10,29 @@ module RedmineExtendedWorkflows::Models
         # Examples:
         #   project.read_only_attribute_names # => ['description', '2']
         #   project.read_only_attribute_names(user) # => []
-        def read_only_attribute_names(user = nil)
-          return [] if user.nil?
-
-          user_real = user || User.current
-          return [] if user_real.admin?
-
-          user_roles_in_project =  user_real.roles_for_project(self)
-
-          return [] if user_roles_in_project.empty?
-
-          Project.get_read_only_attribute_for_roles(user_roles_in_project)
-
+        def read_only_attribute_names(user = User.current)
+          return [] if user.admin?
+          Project.get_read_only_attribute_for_roles(user.roles_for_project(self))
         end
 
         def self.get_read_only_attribute_for_roles(roles)
-          workflow_projects =
-            WorkflowProject.where(
-              :rule => 'readonly', :role_id => roles.map(&:id)
-            )
-
-          workflow_projects_fields = workflow_projects.map(&:field_name)
-          read_only_attributes = []
-
           # Handle cases where roles lack permission to perform update/project.
           workflow_roles_user = roles - Role.excluded_workflow_roles
 
-          # Include this field if it is set to read-only for all consider_workflow roles.
-          workflow_projects_fields.each do |field|
-            occurrences = workflow_projects_fields.count(field)
-          read_only_attributes <<  field if occurrences == workflow_roles_user.count
-          end
-
-          read_only_attributes.uniq
+          WorkflowProject.where(role: workflow_roles_user,
+                                rule: 'readonly')
+                         .pluck(:field_name)
+                         .group_by { |field_name| field_name }
+                         .reject { |k, group| group.length != workflow_roles_user.size }
+                         .keys
         end
+
       end
     end
   end
 end
 Project.send(:include, RedmineExtendedWorkflows::Models::ProjectPatch)
+
 class Project < ActiveRecord::Base
   CORE_FIELDS = %w(name description is_public parent_id inherit_members).freeze
 end
