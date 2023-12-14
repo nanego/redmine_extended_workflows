6dffe9f319d7c5c05a4aa7353d023a87c4e40fff
Yalaeddin <y.alaeddin@adacis.net>
2023-12-14 08:09:14 +0100

    Remplacer la classe par un selector general

--- assets/javascripts/redmine_extended_workflows.js
+++ assets/javascripts/redmine_extended_workflows.js
@@ -1,5 +1,5 @@
 function selectOptionForAll(parent_selector, element_selector, option) {
-  $('.' + parent_selector).find(`select[name^=${element_selector}]`).each(function() {
+  $(parent_selector).find(`select[name^=${element_selector}]`).each(function() {
     $(this).val(option);
   })
 }
